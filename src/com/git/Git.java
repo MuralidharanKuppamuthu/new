09a52121fa7f0d13ebd683877a3980b2f9ccd6b4
muralidharan <kuppamuthumuralidharan@gmail.com>
2022-07-21 11:18:29 +0530

    first commit

---
+++ src/com/git/Git.java
@@ -0,0 +1,34 @@
+package com.git;
+
+public class Git {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		String s = "hungry";
+		
+		if (s.equalsIgnoreCase("hungry")) {
+			
+			System.out.println("eat somrthing");
+			
+			
+		}
+		
+		else {
+			
+			System.out.println("dont eat anything");
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
